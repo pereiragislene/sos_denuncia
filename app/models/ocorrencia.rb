7f4440d579be66e798c01ad8dbc41fea964c6674
Gislene Pereira <gislene01@gmail.com>
2014-11-19 22:06:45 -0300

    Removing accents that were breaking the views

--- app/models/ocorrencia.rb
+++ app/models/ocorrencia.rb
@@ -1,4 +1,4 @@
-class Ocorrencia < ActiveRecord::Base 
+class Ocorrencia < ActiveRecord::Base
 	validates_presence_of :tipo_ocorrencia, :message => " - deve ser preenchido"
 	validates_presence_of :descricao, :message => " - deve ser preenchido"
 	validates_presence_of :nome_vitima, :message => " - deve ser preenchido"
@@ -9,7 +9,7 @@ class Ocorrencia < ActiveRecord::Base
 	validates_presence_of :nome_contato, :message => " - deve ser preenchido"
 	validates_presence_of :telefone_contato, :message => " - deve ser preenchido"
 	validates_format_of :telefone_contato, :with => /\A[\d]+\z/, :message => " - deve ser um numero", :allow_blank => true
-	validates_length_of :telefone_contato, :is => 10, :message => " - deve ter 10 dígitos", :allow_blank => true
+	validates_length_of :telefone_contato, :is => 10, :message => " - deve ter 10 digitos", :allow_blank => true
 
-	TIPOS_DE_OCORRENCIA = ["Assédio", "Homofobia", "Pedofilia", "Racismo", "Violência contra mulher", "Outros"]
+	TIPOS_DE_OCORRENCIA = ["Assedio", "Homofobia", "Pedofilia", "Racismo", "Violencia contra mulher", "Outros"]
 end
